951bda1a69cce31e7e84030627d32cd3dca57159
yolandahe <he.yang7@northeastern.edu>
2024-01-28 23:13:39 -0800

    set up picture on final screen

--- screens/Final.js
+++ screens/Final.js
@@ -1,13 +1,25 @@
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Button, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Color from '../components/Color'
+import Card from '../components/Card'
 
-export default function Final({setScreen, reset}) {
+export default function Final({guessNumber, reset}) {
 
     return (
         <Color>
         <SafeAreaView style={styles.container}>
-        <Button title='Start Again' onPress={reset} />
+            <Text style={styles.header}>Game is Over</Text>
+            <Card>
+                {parseInt(guessNumber) === 1024?
+                <Image 
+                    source={{
+                        uri: "https://picsum.photos/id/1024/100/100",}}
+                    style={styles.image}/> :
+                <Image 
+                    source={require("../assets/sad-face.jpeg")}
+                    style={styles.image}/>}
+                <Button title='Start Again' onPress={reset} />
+            </Card>
         </SafeAreaView>
         </Color>
   )
@@ -20,4 +32,15 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'center',
     },
+
+    header:{
+        color: 'darkblue',
+        fontSize:24,
+        marginBottom:'10%'
+    },
+
+    image: {
+        width: 100,
+        height: 100
+      },
 })
